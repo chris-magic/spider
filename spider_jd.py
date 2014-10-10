2393a876f42c11be995af6bf374450b8fc872e23
林湘粤 <linjiaxiangyue@gmail.com>
2014-10-10 15:32:35 +0800

    add spider jd-taobao-tmail

---
+++ spider_jd.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
+url="http://item.taobao.com/item.htm?spm=a219r.lm869.0.0.NufoDi&id=13878246530&ns=1"
+headers={"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Encoding":"gzip,deflate,sdch",
+        "Accept-Language":"zh-CN,zh;q=0.8",
+        "Cache-Control":"max-age=0",
+        "Connection":"keep-alive"}
+
+image_path = 'image'
+
+
+def spider():
+    html = get_info()
+    price = analyze_info(html)
+    print price
+   # print title
+
+
+def get_info():
+    #print html
+    return html
+
+
+def analyze_jd():
+    #get the page
+    url = raw_input("please input the website:")
+    resp = requests.get(url, headers=headers)
+    html = resp.content
+    html = html.decode('gbk').encode('utf-8')
+    soup = BeautifulSoup(html)
+    #title
+    title = soup.html.head.title
+ #   print title
+
+    #price
+    url_id = url.split('/')[-1].split('.')[0]
+    url_p = 'http://p.3.cn/prices/get?skuid=J_' + url_id
+    resp_price = requests.get(url_p, headers=headers)
+    price = resp_price.content.split(',')[1].split(':')[1]
+    print price
+
+    #image
+    link = soup.find(id="preview").div.img
+
+    link = link['src']
+    print link
+    image_get = requests.get(url=link, headers=headers)
+    img_content = image_get.content
+
+    file_name = str(int(time.time()))+'.jpg'
+    file_path = os.path.join(image_path,file_name)
+    image = open(file_path,'wb')
+    image.write(img_content)
+    image.close()
+#    html = html.decode('gbk').encode('utf-8')
+ #   soup = BeautifulSoup(html)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    analyze_jd()
